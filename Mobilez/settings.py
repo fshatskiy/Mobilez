9f1dfa3de4a0f1d7dbb10dbece39dc498e73d47c
Rody96 <HE201493@students.ephec.be>
2020-05-25 00:56:51 +0200

    modifs

--- Mobilez/settings.py
+++ Mobilez/settings.py
@@ -212,4 +212,4 @@ django_heroku.settings(locals())
 
 import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+DATABASES['default'] = dj_database_url.config(default='mongodb+srv://herokudb:Temporary72%21@mobilezcluster1-piwai.mongodb.net/test?retryWrites=true&w=majority')
