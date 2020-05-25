861d8cf88caf8604e15486a0cd807898c700249f
Rody96 <HE201493@students.ephec.be>
2020-05-25 17:17:32 +0200

    test db

--- Mobilez/settings.py
+++ Mobilez/settings.py
@@ -144,16 +144,6 @@ WSGI_APPLICATION = 'Mobilez.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-       'NAME': 'test',
-       'USER': 'root',
-       'PASSWORD':'root',
-        'HOST': '127.0.0.1',
-       'PORT': '3307',
-   }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
