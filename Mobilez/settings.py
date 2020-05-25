2d90390c5ca76c37c2ee98d705c8c2a0e3a1d3fd
Rody96 <HE201493@students.ephec.be>
2020-05-25 23:47:37 +0200

    modifs settings.py

--- Mobilez/settings.py
+++ Mobilez/settings.py
@@ -195,7 +195,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'Mobilez/static'),
+    os.path.join(BASE_DIR, 'Mobilez/static/css'),
 )
 
 CORS_ORIGIN_ALLOW_ALL = True
