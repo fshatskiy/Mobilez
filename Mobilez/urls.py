b2753a8faf77756bc6612ea3653671dd501d18a6
Rody96 <HE201493@students.ephec.be>
2020-05-26 12:06:35 +0200

    modifs urls.py

--- Mobilez/urls.py
+++ Mobilez/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path('', include('allauth.urls')),
     path('accueil/', include('accueil.urls')),
     path('map/', include('map.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
