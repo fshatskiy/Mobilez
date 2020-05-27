234e534959c383e9aabd41903ff9a991bf3f8788
Rody96 <HE201493@students.ephec.be>
2020-05-27 13:28:03 +0200

    changement adresse mail hôte

--- Mobilez/settings.py
+++ Mobilez/settings.py
@@ -99,8 +99,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = DEFAULT_FROM_EMAIL = 'ydor9928@gmail.com'
-EMAIL_HOST_PASSWORD = 'PaulGeorge13'
+EMAIL_HOST_USER = DEFAULT_FROM_EMAIL = 'noreplymobilez@gmail.com'
+EMAIL_HOST_PASSWORD = 'Temporary72!'
 
 # à modifier en cas de besoin
 ACCOUNT_EMAIL_VERIFICATION = True
